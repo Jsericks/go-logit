3b6a937c813643d7954c7759983c5e6f9e29e216
Brandon Bennett <brandonabennett24@gmail.com>
2017-02-21 20:27:15 -0700

    Add atomic assignments and reads to logLevel (#2)
    * Add atomic assignments and reads to logLevel
    
    * Combine the getters and name everything properly
    
    * Add handling of negative int in SetLogLevel
    
    * Now has getters and setters both by name and by number
    
    * Return errors if incorrect level and add constants for level options
    
    * Change const to all caps
    
    * Golint change on error string

--- log.go
+++ log.go
@@ -1,58 +1,120 @@
 package logit
 
 import (
+	"errors"
 	"log"
 	"os"
 	"strings"
+	"sync/atomic"
 )
 
-var LogLevel int
+const (
+	// DEBUG doesn't filter any logs
+	DEBUG int = 0
+	// INFO filters only Debug logs
+	INFO int = 1
+	// WARN filters Info and Debug logs
+	WARN int = 2
+	// ERROR prints Error and Fatal logs
+	ERROR int = 3
+	// FATAL prints only Fatal logs
+	FATAL int = 4
+)
+
+var (
+	logLevel      int32
+	errWrongLevel = errors.New("incorrect log level, options are DEBUG|INFO|WARN|ERROR|FATAL")
+)
 
 func init() {
-	logLevel := strings.ToLower(os.Getenv("LOG_LEVEL"))
-
-	switch logLevel {
-	case "debug":
-		LogLevel = 0
-	case "info":
-		LogLevel = 1
-	case "warn":
-		LogLevel = 2
-	case "error":
-		LogLevel = 3
-	case "fatal":
-		LogLevel = 4
-	default:
-		LogLevel = 3
+	initialLevel := strings.ToUpper(os.Getenv("LOG_LEVEL"))
+	err := setLogLevelByName(initialLevel)
+	if err != nil {
+		panic(err)
 	}
 }
 
+// Debug prints the format string with the prefix of "DEBUG:"
 func Debug(format string, v ...interface{}) {
-	if LogLevel <= 0 {
+	if LogLevel() <= 0 {
 		log.Printf("DEBUG:"+format, v...)
 	}
 }
 
+// Info prints the format string with the prefix of "INFO:"
 func Info(format string, v ...interface{}) {
-	if LogLevel <= 1 {
+	if LogLevel() <= 1 {
 		log.Printf("INFO:"+format, v...)
 	}
 }
 
+// Warn prints the format string with the prefix of "WARN:"
 func Warn(format string, v ...interface{}) {
-	if LogLevel <= 2 {
+	if LogLevel() <= 2 {
 		log.Printf("WARN:"+format, v...)
 	}
 }
 
+// Error prints the format string with the prefix of "ERROR:"
 func Error(format string, v ...interface{}) {
-	if LogLevel <= 3 {
+	if LogLevel() <= 3 {
 		log.Printf("ERROR:"+format, v...)
 	}
 }
 
+// Fatal prints the format string with the prefix of "FATAL:"
 func Fatal(format string, v ...interface{}) {
-	if LogLevel <= 4 {
+	if LogLevel() <= 4 {
 		log.Printf("FATAL:"+format, v...)
 	}
+}
+
+func setLogLevelByName(newLevel string) error {
+	switch newLevel {
+	case "DEBUG":
+		SetLogLevel(0)
+	case "INFO":
+		SetLogLevel(1)
+	case "WARN":
+		SetLogLevel(2)
+	case "ERROR":
+		SetLogLevel(3)
+	case "FATAL":
+		SetLogLevel(4)
+	default:
+		return errWrongLevel
+	}
+	return nil
+}
+
+// SetLogLevel takes the logLevel int and changes the level in a thread-safe way
+func SetLogLevel(newLevel int) error {
+	if newLevel < 0 || newLevel > 4 {
+		return errWrongLevel
+	}
+	atomic.StoreInt32(&logLevel, int32(newLevel))
+	return nil
+}
+
+// LogLevelName reads the level's name in a thread-safe way
+func LogLevelName() string {
+	switch LogLevel() {
+	case 0:
+		return "DEBUG"
+	case 1:
+		return "INFO"
+	case 2:
+		return "WARN"
+	case 3:
+		return "ERROR"
+	case 4:
+		return "FATAL"
+	default:
+		return "Log level error"
+	}
+}
+
+// LogLevel reads the level in a thread-safe way
+func LogLevel() int {
+	return int(atomic.LoadInt32(&logLevel))
 }
